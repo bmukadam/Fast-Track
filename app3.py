7e80349735ea63be9e188d3fc9d4cd3f3a242c53
Bilal Mukadam <bmukadam@princeton.ed>
2018-04-13 11:02:47 -0400

    attempting sending data without decorator route

--- app3.py
+++ app3.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/FastTrackPython', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hello():
 	#if request.method == 'POST':
 	src = str(request.form['src'])
