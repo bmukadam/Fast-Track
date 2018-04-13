41dd39794405a5d4897835512f9ef758d5ce1060
Bilal Mukadam <bmukadam@princeton.ed>
2018-04-13 10:52:57 -0400

    introducing decorator in python code as /FastTrackPython

--- app3.py
+++ app3.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
- 
+
+@app.route('/FastTrackPython', methods=['GET', 'POST'])
+def hello():
+	#if request.method == 'POST':
+	src = str(request.form['src'])
+	dst = str(request.form['dst'])
+	output = "Length of src was: " + str(len(src)) + " and recieved data was: " + src + "\n" + 	"Length of dst was: " + str(len(dst)) + " and recieved data was: " + dst
+	#return output
+	 
 #, methods=['GET', 'POST']
 @app.route('/')
 def index():
