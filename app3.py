4c06c55e19254228eb6d0a31dfe662488e3f5b53
Bilal Mukadam <bmukadam@princeton.ed>
2018-04-13 01:21:44 -0400

    Reached Working version with python backend in last commit. Added button interactivity

--- app3.py
+++ app3.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
  
 #, methods=['GET', 'POST']
+@app.route('/FastTrackPython', methods=['GET', 'POST'])
+def hello():
+	#if request.method == 'POST':
+	src = str(request.form['src'])
+	dst = str(request.form['dst'])
+	output = "Length of src was: " + str(len(src)) + " and recieved data was: " + src + "\n" + 	"Length of dst was: " + str(len(dst)) + " and recieved data was: " + dst
+	return output
+
 @app.route('/')
 def index():
 	return render_template('index.html')
